1413718f76c3dcf16ebed42658f78670c8858cba
vikyjaware <vikyjaware@gmail.com>
2022-02-03 16:54:08 +0530

    [Viky] Add : Folder structure added

--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace AddressBook
 {
-    internal class AddressBookMain
+    public class AddressBookMain
     {
+        List<Contacts> addressbook=new List<Contacts>();
+        public void CreateContact()
+        {
+            Contacts contacts = new Contacts();
+            Console.WriteLine("Enter FirstName");
+            contacts.FirstName = Console.ReadLine();
+        }
     }
 }
