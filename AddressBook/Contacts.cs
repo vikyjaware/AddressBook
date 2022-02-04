803696c402b00be087cb8ce403eb0442e646d257
vikyjaware <vikyjaware@gmail.com>
2022-02-04 09:21:54 +0530

    [Viky] Add : Folder structure added

--- AddressBook/Contacts.cs
+++ AddressBook/Contacts.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    internal class Contacts
+    public class Contacts
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public void CreateContact()
+        {
+            Console.WriteLine("Enter the First Name");
+            this.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name");
+            this.LastName = Console.ReadLine();
+            Console.WriteLine("Enter the Address");
+            this.Address = Console.ReadLine();
+            Console.WriteLine("Enter the City");
+            this.City = Console.ReadLine();
+            Console.WriteLine("Enter the State");
+            this.State = Console.ReadLine();
+            Console.WriteLine("Enter the Zip");
+            this.Zip =Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Enter the PhoneNumber");
+            this.PhoneNumber = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Enter the Email");
+            this.Email = Console.ReadLine();
+
+            Console.WriteLine(FirstName + LastName + " Contact Created");
+        }
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBook;
 Console.WriteLine("Welcome to the Address Book problem statement!");
+Contacts Addressbook =new Contacts();
+Addressbook.CreateContact();
