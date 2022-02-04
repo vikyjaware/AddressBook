d21b9c6b5d9977af31a301e4dfde5cfdb578e5bd
vikyjaware <vikyjaware@gmail.com>
2022-02-04 16:07:49 +0530

    [Viky] Add : Folder structure added

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -2,5 +2,33 @@
 using AddressBook;
 Console.WriteLine("Welcome to the Address Book problem statement!");
 
-AddressBookMain AddressBook=new AddressBookMain();
-AddressBook.AddNewContact();
+bool var = true;
+            AddressBookMain addressBook = new AddressBookMain();
+while (var)
+{
+Console.WriteLine("Select the operation from following..\n "
+    + "Enter 1 for Adding a new contact\n "
+    + "Enter 2 for editing an existing contact\n "
+    + "Enter 3 for deleting an existing contact");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            addressBook.AddNewContact();
+            break;
+        case 2:
+            Console.WriteLine("Enter First Name To Edit Contact : ");
+            string FirstName = Console.ReadLine();
+            addressBook.EditContact(FirstName);
+            break;
+        case 3:
+            Console.WriteLine("Enter First Name to delete Contact : ");
+            string FirstNametoDelete= Console.ReadLine();
+            addressBook.DeleteContact(FirstNametoDelete);
+            break ;
+        default:
+            var=false;
+            Console.WriteLine("Enter Correct Option...");
+            break;
+    }
+}
